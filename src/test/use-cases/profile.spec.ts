4cadd561e94c3406442b557be6d4e53aa05a7161
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-01 22:42:31 -0300

    [0.1.5] fix firsts test with checkin in profile

--- src/test/use-cases/profile.spec.ts
+++ src/test/use-cases/profile.spec.ts
@@ -1,33 +1,62 @@
+import { InMemoryCheckInRepository } from 'repositories/check-ins/in-memory-check-in-repository'
 import { InMemoryUserRepository } from 'repositories/user/in-memory-user-repository'
 import { ProfileUseCase } from 'use-cases/profile'
 import { expect, describe, it, beforeEach } from 'vitest'
 
 // set variables to use in tests
 let userRepository: InMemoryUserRepository
+let checkinsRepository: InMemoryCheckInRepository
 let sut: ProfileUseCase
 describe('register use Case', () => {
   beforeEach(() => {
     // Clear the in-memory user repository before each test
     userRepository = new InMemoryUserRepository()
-    sut = new ProfileUseCase(userRepository)
+    checkinsRepository = new InMemoryCheckInRepository()
+    sut = new ProfileUseCase(userRepository, checkinsRepository)
     userRepository.clear()
   })
-  it('should be able to get the user infos in profile', async () => {
+  it('should be able to get the user infos in profile without checkin', async () => {
     const user = await userRepository.create({
       name: 'John Doe',
       password_hash: '123456',
       email: 'johnDoe@email.com',
     })
 
-    const userProfile = await sut.execute({
+    const profile = await sut.execute({
       id: user.id,
     })
-    expect(userProfile).toEqual({
+    expect(profile).toEqual({
       user: expect.objectContaining({
         id: user.id,
-      })
+      }),
+      quantityCheckIns: 0,
     })
-    expect(userProfile).not.toBeNull()
+    expect(profile).not.toBeNull()
+  })
+  it('should be able to get the user infos in profile with one checkin', async () => {
+    const user = await userRepository.create({
+      name: 'John Doe',
+      password_hash: '123456',
+      email: 'johnDoe@email.com',
+    })
+
+    await checkinsRepository.create({
+      gymId: 'gym-1',
+      userId: user.id,
+      validatedAt: new Date(),
+      createdAt: new Date(),
+    })
+
+    const profile = await sut.execute({
+      id: user.id,
+    })
+    expect(profile).toEqual({
+      user: expect.objectContaining({
+        id: user.id,
+      }),
+      quantityCheckIns: 1,
+    })
+    expect(profile).not.toBeNull()
   })
 
   it('should throw error when not found user profile ', async () => {
