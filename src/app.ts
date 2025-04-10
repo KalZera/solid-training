911d05073f0da2a8c993fa3b9e985942aab1cc79
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-09 22:28:58 -0300

    [0.3.2] add middleware to accept jwt

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
-import fastify from 'fastify'
+import { env } from 'env'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { routes } from 'http/routes'
 
 export const app = fastify()
 
+app.decorate('authenticate', async function (request: FastifyRequest, reply: FastifyReply) {
+  try {
+    await request.jwtVerify()
+  } catch (err) {
+    reply.send(err)
+  }
+})
+
 app.register(fastifyJwt, {
-  secret: 'secret',
+  secret: env.JWT_SECRET_KEY,
   cookie: {
     cookieName: 'refreshToken',
     signed: false,
--- src/http/controller/profile.ts
+++ src/http/controller/profile.ts
@@ -1,20 +1,14 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 import { makeProfileUseCaseFactory } from 'factories/use-cases/user/make-profile-factory'
 
 export async function profile (request:FastifyRequest, reply:FastifyReply) {
-  const profileBodySchema = z.object({
-    id: z.string(),
-  })
-
-  const { id } = profileBodySchema.parse(request.params)
-
+  const { sub: id } = request.user
   try {
     const getUserProfileUseCase = makeProfileUseCaseFactory()
 
-    const user = await getUserProfileUseCase.execute({ id })
+    const { user } = await getUserProfileUseCase.execute({ id })
 
-    return reply.status(201).send({ user })
+    return reply.status(201).send({ ...user, password_hash: undefined })
   } catch (error) {
     return reply.status(409).send({ message: (error as Error).message })
   }
