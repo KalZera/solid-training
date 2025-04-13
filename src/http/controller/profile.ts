4084b5a3b1ff416b3798f2a9a1141e17e0b411b3
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-12 21:05:35 -0300

    [0.3.10] create and fix profile controller tests

--- src/http/controller/profile.ts
+++ src/http/controller/profile.ts
@@ -8,7 +8,7 @@ export async function profile (request:FastifyRequest, reply:FastifyReply) {
 
     const { user } = await getUserProfileUseCase.execute({ id })
 
-    return reply.status(201).send({ ...user, password_hash: undefined })
+    return reply.status(200).send({ ...user, password_hash: undefined })
   } catch (error) {
     return reply.status(409).send({ message: (error as Error).message })
   }
---
+++ src/test/controllers/profile.spec.ts
@@ -0,0 +1,29 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from 'app'
+import request from 'supertest'
+import { createAndAuthenticateUser } from 'utils/test/create-and-authenticate'
+
+describe('Profile e2e test', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to get profile infos', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const response = await request(app.server).get(
+      '/profile'
+    ).set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        name: 'John Doe',
+        email: 'Johndoe@email.com'
+      })
+    )
+    expect(response.body).not.toHaveProperty('password_hash')
+  })
+})
