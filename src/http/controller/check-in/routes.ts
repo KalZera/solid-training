cd71f46d304371a7921c2ec81581cd9d1096138c
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-14 22:48:16 -0300

    [0.4.2] add user role validation to check-in and gym routes

--- src/http/controller/check-in/routes.ts
+++ src/http/controller/check-in/routes.ts
@@ -1,15 +1,20 @@
 import type { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middleware/jwt-middleware'
+import { verifyUserRole } from '../../middleware/verify-user-role'
 import { createCheckIn } from './create-check-in'
 import { getCheckIn } from './get-check-in-by-user'
 import { metricsCheckIn } from './metrics-check-in'
 import { validateCheckIn } from './validate-check-in'
 
-export async function checkInRoutes (app:FastifyInstance) {
+export async function checkInRoutes (app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.get('/check-ins', getCheckIn)
   app.get('/check-ins/metrics', metricsCheckIn)
-  app.patch('/check-ins/:checkInId/validate', validateCheckIn)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validateCheckIn
+  )
 
   app.post('/gyms/:gymId/check-ins', createCheckIn)
 }
--- src/http/controller/gym/routes.ts
+++ src/http/controller/gym/routes.ts
@@ -3,10 +3,11 @@ import { verifyJWT } from '../../middleware/jwt-middleware'
 import { createGym } from './create-gym'
 import { searchGym } from './search-gyms'
 import { nearByGym } from './near-by-gyms'
+import { verifyUserRole } from 'http/middleware/verify-user-role'
 
 export async function gymRoutes (app:FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
-  app.post('/gyms', createGym)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, createGym)
   app.get('/gyms', searchGym)
   app.get('/gyms/nearby', nearByGym)
 }
