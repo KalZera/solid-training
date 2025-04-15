1b61e04516b2aa0b4aaa8bbc513ad30823609070
Alexander Karuzoo <kalex.codr@gmail.com>
2025-04-14 21:42:25 -0300

    [0.3.22] add e2e tests for metrics check-in and validation functionality

---
+++ src/test/controllers/validate-check-in.spec.ts
@@ -0,0 +1,48 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from 'app'
+import request from 'supertest'
+import { createAndAuthenticateUser } from 'utils/test/create-and-authenticate'
+import { prisma } from 'lib/prisma'
+
+describe('Validate CheckIn e2e test', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to validate', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Gym Test',
+        latitude: -23.5505,
+        longitude: -46.6333,
+      },
+    })
+
+    let checkIn = await prisma.checkIn.create({
+      data: {
+        userId: user.id,
+        gymId: gym.id,
+      },
+    })
+
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkIn.id}/validate`)
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toEqual(204)
+
+    checkIn = await prisma.checkIn.findFirstOrThrow({
+      where: {
+        id: checkIn.id,
+      },
+    })
+
+    expect(checkIn.validatedAt).toEqual(expect.any(Date))
+  })
+})
